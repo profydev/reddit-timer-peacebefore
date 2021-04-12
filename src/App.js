7ab17a0bfdd8387a1112cbe11400ee90c9469b0b
peacebefore <42794888+peacebefore@users.noreply.github.com>
2021-04-12 15:33:58 -0700

    added react-router-dom and switch routes to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Switch, Route } from 'react-router-dom';
 
-import GlobalStyles from './infrastructure/theme/globalstyles';
+import GlobalStyles from './styles/theme/globalstyles';
 
 export default function App() {
   return (
     <div>
       <GlobalStyles />
+      <Switch>
+        <Route exact path="/" render={() => null} />
+        <Route exact path="/search" render={() => null} />
+      </Switch>
+
       <h1>No reactions to your Reddit posts?</h1>
       <p>
         Great timing, great results! Find the best time to post on your
