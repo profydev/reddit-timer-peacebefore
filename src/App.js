8b7bf59413ecb450c0f523441eeda10c023d262d
Priya <42794888+peacebefore@users.noreply.github.com>
2021-05-01 20:41:15 -0700

    Create app skeleton (#1)
    * installed bitter and montserrat fonts, addeds styled components and sass packages
    
    * set up infrastructure folder for theme and fonts
    
    * set up infrastructure folder with theme and fonts
    
    * adding global styles
    
    * added react-router-dom and switch routes to app
    
    * fixing error in path to font file for index.css
    
    * Change app title and meta description

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Switch, Route } from 'react-router-dom';
 
-function App() {
+import GlobalStyles from './styles/theme/globalstyles';
+
+export default function App() {
   return (
     <div>
-      App Placeholder
+      <GlobalStyles />
+      <Switch>
+        <Route exact path="/" render={() => null} />
+        <Route exact path="/search" render={() => null} />
+      </Switch>
+
+      <h1>No reactions to your Reddit posts?</h1>
+      <p>
+        Great timing, great results! Find the best time to post on your
+        subreddit.
+      </p>
     </div>
   );
 }
-
-export default App;
