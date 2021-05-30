647a56633894beddd017c4df4491bbe666fbe2f0
peacebefore <42794888+peacebefore@users.noreply.github.com>
2021-05-29 23:44:12 -0700

    resolving conflicts

--- src/App.js
+++ src/App.js
@@ -4,15 +4,10 @@ import { Normalize } from 'styled-normalize';
 import Header from './components/header/header.component';
 import GlobalStyles from './styles/theme/globalstyles';
 
-<<<<<<< HEAD
 import HomePage from './pages/home/home.page';
 import SearchPage from './pages/search.page';
 import TermsPage from './pages/terms.page';
 import Footer from './components/footer/footer.component';
-=======
-import HomePage from './pages/home.page';
-import SearchPage from './pages/search.page';
->>>>>>> 2553374f6fa1d627501b1dff25d3a3a11381881f
 
 function App() {
   return (
@@ -27,15 +22,11 @@ function App() {
         <Route exact path="/search/javascript">
           <SearchPage />
         </Route>
-<<<<<<< HEAD
         <Route exact path="/terms">
           <TermsPage />
         </Route>
       </Switch>
       <Footer />
-=======
-      </Switch>
->>>>>>> 2553374f6fa1d627501b1dff25d3a3a11381881f
     </Router>
   );
 }
